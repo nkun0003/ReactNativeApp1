ad71bda118c9106e6816d7130bd88c6ed94c783e
Rocien Nkunga <rociennkunga@gmail.com>
2024-09-23 00:06:41 -0400

    Added pressable to the owner texts

--- pages/OwnersPage.js
+++ pages/OwnersPage.js
@@ -7,27 +7,21 @@ export default function ListPage({ navigation }) {
       <FlatList
         data={data}
         renderItem={({ item }) => (
-          <Pressable onPress={() => alert('User is selected.')}>
-            <Text
-              style={{
-                padding: 16,
-                fontSize: 20,
-                backgroundColor: 'blue',
-                margin: 2,
-                color: 'white'
-              }}>
-              {item.title}
-            </Text>
+          <Pressable
+            style={styles.buttonPress} // Style for each Pressable
+            onPress={() => alert(`${item.title} is Clicked.`)} // Alert the specific user's name
+          >
+            <Text style={styles.itemText}>{item.title}</Text>
           </Pressable>
         )}
         keyExtractor={(item) => item.id}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonHome}>
-          <Button title="Home Page" onPress={() => navigation.navigate('Home')}></Button>
+          <Button title="Home Page" onPress={() => navigation.navigate('Home')} />
         </View>
         <View style={styles.buttonItems}>
-          <Button title="Cars Page" onPress={() => navigation.navigate('Cars')}></Button>
+          <Button title="Cars Page" onPress={() => navigation.navigate('Cars')} />
         </View>
       </View>
     </SafeAreaView>
@@ -35,70 +29,39 @@ export default function ListPage({ navigation }) {
 }
 
 const data = [
-  {
-    id: '1',
-    title: 'Smith'
-  },
-  {
-    id: '2',
-    title: 'John'
-  },
-  {
-    id: '3',
-    title: 'Sarah'
-  },
-  {
-    id: '4',
-    title: 'Ross'
-  },
-  {
-    id: '5',
-    title: 'Kate'
-  },
-  {
-    id: '6',
-    title: 'Jossy'
-  },
-  {
-    id: '7',
-    title: 'Pierre'
-  },
-  {
-    id: '8',
-    title: 'Katie'
-  },
-  {
-    id: '9',
-    title: 'Ross'
-  },
-  {
-    id: '10',
-    title: 'Sephy'
-  },
-  {
-    id: '11',
-    title: 'Kenan'
-  },
-  {
-    id: '12',
-    title: 'Treah'
-  },
-  {
-    id: '13',
-    title: 'Jack'
-  },
-  {
-    id: '14',
-    title: 'Gedeon'
-  }
+  { id: '1', title: 'Smith' },
+  { id: '2', title: 'John' },
+  { id: '3', title: 'Sarah' },
+  { id: '4', title: 'Ross' },
+  { id: '5', title: 'Kate' },
+  { id: '6', title: 'Jossy' },
+  { id: '7', title: 'Pierre' },
+  { id: '8', title: 'Katie' },
+  { id: '9', title: 'Ross' },
+  { id: '10', title: 'Sephy' },
+  { id: '11', title: 'Kenan' },
+  { id: '12', title: 'Treah' },
+  { id: '13', title: 'Jack' },
+  { id: '14', title: 'Gedeon' }
 ];
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: "center",
     justifyContent: 'center'
-    // paddingTop: 48,
+  },
+  buttonPress: {
+    backgroundColor: 'blue',
+    marginVertical: 5,
+    marginHorizontal: 5,
+    borderRadius: 20,
+    padding: 10
+  },
+  itemText: {
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center'
   },
   buttonsContainer: {
     flexDirection: 'row',
